463bfe8e1720ac590025d96a88eadbbd36c51773
Dmitry Misevra <shumi199@yandex.ru>
2023-04-13 23:41:32 +0600

    feat: Создать утилитарный класс Managers, который отвечает за создание объектов типа TaskManager и HisttoryManager

---
+++ src/helpers/Managers.java
@@ -0,0 +1,19 @@
+package helpers;
+
+import service.HistoryManager;
+import service.InMemoryHistoryManager;
+import service.InMemoryTaskManager;
+import service.TaskManager;
+
+public class Managers {
+
+/*     метод getDefault() будет возвращать необходимый субкласс TaskManager. пока по умолчанию поставил
+     InMemoryTaskManager(); */
+    public static TaskManager getDefault() {
+        return new InMemoryTaskManager();
+    }
+
+    public static HistoryManager getDefaultHistory () {
+        return new InMemoryHistoryManager();
+    }
+}
