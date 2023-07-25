ff84ec89cc22f93de84bcd328ac71fe750d9ca8d
Dmitry Misevra <shumi199@yandex.ru>
2023-07-26 00:26:01 +0600

    refactor: добавить менеджер httpTaskServer

---
+++ src/service/HttpTaskManager.java
@@ -0,0 +1,83 @@
+package service;
+
+import client.KVTaskClient;
+import com.google.gson.Gson;
+import helpers.Managers;
+import module.Epic;
+import module.Subtask;
+import module.Task;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class HttpTaskManager extends FileBackedTasksManager implements TaskManager {
+
+    final KVTaskClient client;
+    final Gson gson = Managers.getGson();
+
+    public HttpTaskManager(String url) throws IOException, InterruptedException {
+        super(null);
+        client = new KVTaskClient(url);
+    }
+
+    /* метод загружает данные из сервера  */
+    public static HttpTaskManager loadFromServer(String url) {
+        try {
+            HttpTaskManager httpTaskManager = new HttpTaskManager(url);
+            KVTaskClient client = new KVTaskClient(url);
+            Gson gson = Managers.getGson();
+
+            Task[] taskArray = gson.fromJson(client.load("task"), Task[].class);
+            Epic[] epicArray = gson.fromJson(client.load("epic"), Epic[].class);
+            Subtask[] subtaskArray = gson.fromJson(client.load("subtask"), Subtask[].class);
+            Integer[] historyArray = gson.fromJson(client.load("history"), Integer[].class);
+
+            Arrays.asList(taskArray).forEach(task -> {
+                httpTaskManager.tasks.put(task.getId(), task);
+                httpTaskManager.updateTask(task);
+            });
+
+            Arrays.asList(epicArray).forEach(epic -> httpTaskManager.epics.put(epic.getId(), epic));
+
+            Arrays.asList(subtaskArray).forEach(subtask -> {
+                httpTaskManager.subtasks.put(subtask.getId(), subtask);
+                httpTaskManager.updateSubtask(subtask);
+            });
+
+                Arrays.asList(historyArray).forEach(integer -> {
+                    if (httpTaskManager.tasks.containsKey(integer)) {
+                        Task task = httpTaskManager.tasks.get(integer);
+                        httpTaskManager.historyManager.add(task);
+                    } else if (httpTaskManager.subtasks.containsKey(integer)) {
+                        Subtask subtask = httpTaskManager.subtasks.get(integer);
+                        httpTaskManager.historyManager.add(subtask);
+                    } else if (httpTaskManager.epics.containsKey(integer)) {
+                        Epic epic = httpTaskManager.epics.get(integer);
+                        httpTaskManager.historyManager.add(epic);
+                    }
+                });
+            return httpTaskManager;
+        } catch (IOException | InterruptedException e) {
+            throw new ManagerSaveException("Ошибка чтения c cервера: " + e.getMessage());
+        }
+    }
+
+    /* переопределенный метод сохраняет данные на сервер */
+    @Override
+    protected void save() {
+        try {
+            String jsonTaskArray = gson.toJson(tasks.values());
+            String jsonEpicArray = gson.toJson(epics.values());
+            String jsonSubtaskArray = gson.toJson(subtasks.values());
+            Integer[] historyArray = getHistory().stream().map(Task::getId).toArray(Integer[]::new);
+            String jsonHistoryArray = gson.toJson(historyArray);
+            client.put("task", jsonTaskArray);
+            client.put("epic", jsonEpicArray);
+            client.put("subtask", jsonSubtaskArray);
+            client.put("history", jsonHistoryArray);
+        } catch (IOException | InterruptedException e) {
+            throw new ManagerSaveException("Ошибка записи на сервер: " + e.getMessage());
+        }
+    }
+}
